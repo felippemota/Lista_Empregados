26a97b885125103ff167d7b9bed66d98ca206c6d
FELIPPE SANTOS MOTA MOTA <felippemota@outlook.com.br>
2022-04-21 13:58:29 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_Empregados/Empregados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Acrescimo_de_Salario
+{
+    class Empregados
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+
+        public Empregados(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AcrescentarSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + " , " + Salario;
+        }
+
+    }
+}
---
+++ Lista_Empregados/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lista_Acrescimo_de_Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos empregados serão registrados?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Empregados> lista = new List<Empregados>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Empregado #" + i + ":");
+                Console.Write("Insira sua Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Insira seu Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Insira o salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                lista.Add(new Empregados(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Digite a ID do funcionário que terá o salário reajustado: ");
+            int procura_id = int.Parse(Console.ReadLine());
+
+            Empregados emp = lista.Find(x => x.Id == procura_id);
+            if (emp != null)
+            {
+                Console.Write("Entre com a porcentagem: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.AcrescentarSalario(percentage);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada dos empregados:");
+            foreach (Empregados obj in lista)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
+
